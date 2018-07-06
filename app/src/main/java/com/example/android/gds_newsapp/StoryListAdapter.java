579d3fd75e075f0852dd42cab8bddf876a293080
Alin-Gabriel Ionita <alan@hiddenltd.com>
2018-07-06 23:27:35 +0100

    feat(clickEvent): when clicked take the user to the story URL via a browser intent

--- app/src/main/java/com/example/android/gds_newsapp/StoryListAdapter.java
+++ app/src/main/java/com/example/android/gds_newsapp/StoryListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.gds_newsapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -59,6 +61,16 @@ public class StoryListAdapter extends ArrayAdapter<Story> {
                         itemPublishDate.setText(datePubHolder);
                     }
                 }
+
+                // Set onClickListener that triggers a browser intent with each story url
+                convertView.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Trigger a browser intent with the stored URL
+                        getContext()
+                                .startActivity(new Intent(Intent.ACTION_VIEW)
+                                        .setData(Uri.parse(currentStory.getWebUrl())));
+                    }
+                });
             }
         }
         return convertView;
