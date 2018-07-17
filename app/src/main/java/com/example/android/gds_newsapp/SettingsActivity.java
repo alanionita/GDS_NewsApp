c7dda20d82a9e9359b640201de422a742d755459
Alin-Gabriel Ionita <alan@hiddenltd.com>
2018-07-17 22:53:04 +0100

    feat(preferences): adds preference summary for minNewsStories

--- app/src/main/java/com/example/android/gds_newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/gds_newsapp/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.gds_newsapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,11 +16,28 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class NewsPreferenceFragment extends PreferenceFragment {
+    public static class NewsPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minNewsStories = findPreference(getString(R.string.settings_min_stories_key));
+            bindPreferenceSummaryToValue(minNewsStories);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
         }
     }
 }
